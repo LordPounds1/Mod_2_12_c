c1db8bc196d20c1547649bcdb6730d392f5db0a1
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 22:08:53 +0600

    Add project files.

---
+++ Mod_2_12_c/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("c. Введите значение a (a < 50): ");
+        int a = int.Parse(Console.ReadLine());
+
+        if (a < 50)
+        {
+            Console.WriteLine($"Третьи степени всех целых чисел от {a} до 50:");
+            for (int i = a; i <= 50; i++)
+            {
+                Console.WriteLine(Math.Pow(i, 3));
+            }
+        }
+        else
+        {
+            Console.WriteLine("Значение a должно быть меньше 50.");
+        }
+    }
+}
